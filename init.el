fec18381eeba6134285e51fefe8df10a4aa5b425
vj <venkatesh.jatla@gmail.com>
2020-11-05 15:50:56 -0700

    Added support for multiple themes based on system name

--- init.el
+++ init.el
@@ -5,9 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(conda-anaconda-home "/home/vj/anaconda3")
  '(custom-safe-themes
-   '("37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" "b721b84f75419ba5a4176f3dbb23264439bc62c1115bc1e05dc536679ccb9f3c" default))
+   (quote
+    ("f2c35f8562f6a1e5b3f4c543d5ff8f24100fae1da29aeb1864bbc17758f52b70" "37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" "b721b84f75419ba5a4176f3dbb23264439bc62c1115bc1e05dc536679ccb9f3c" default)))
  '(package-selected-packages
-   '(magit swiper-helm git-gutter helm-projectile helm counsel-projectile treemacs-projectile sublimity company impatient-mode simple-httpd markdown-mode nord-theme ivy lsp-mode xah-fly-keys window-jump which-key use-package treemacs spacegray-theme simpleclip projectile org-bullets modalka evil elpy counsel conda)))
+   (quote
+    (magit swiper-helm git-gutter helm-projectile helm counsel-projectile treemacs-projectile sublimity company impatient-mode simple-httpd markdown-mode nord-theme ivy lsp-mode xah-fly-keys window-jump which-key use-package treemacs spacegray-theme simpleclip projectile org-bullets modalka evil elpy counsel conda))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -41,7 +43,14 @@
  ;; Linux
  ((string-equal system-type "gnu/linux")
   (load "/home/vj/.emacs.d/the_call.el")
-  (set-face-attribute 'default nil :family "Fira Code" :height 150)
-    (load-theme 'nord)
+  (cond
+   ((string-equal system-name "precision7920")
+    (load-theme 'zenburn)
+    (set-face-attribute 'default nil :family "DejaVu Sans Mono" :height 130)
+    )
+   (string-equal system-name "bazul")
+   (load-theme 'nord)
+   (set-face-attribute 'default nil :family "Fira Code" :height 150)
+   )
     (add-to-list 'exec-path "/home/vj/Software/node-v14.15.0-linux-x64/")
     (add-to-list 'exec-path "/home/vj/Software/node-v14.15.0-linux-x64/bin/")))
