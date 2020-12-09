c77d58d4090c8364b4c3735e93ec40bbb7a34e34
vj <venkatesh.jatla@gmail.com>
2020-12-09 16:35:43 -0700

    F5 now compiles org file

--- lisp/org-settings.el
+++ lisp/org-settings.el
@@ -1,8 +1,12 @@
 ;; Publish haq project shortcut
 (load "haq")
-(defun org-publish-haq ()
+(defun org-publish-haq-all ()
   (interactive)
   (org-publish "haq" t)
+  )
+(defun org-publish-haq ()
+  (interactive)
+  (org-publish "haq")
   )
 
 ;; org-agenda files
@@ -10,12 +14,12 @@
 
  ((string-equal system-type "windows-nt")
   ;; Windows settings go here
-  (setq org-agenda-files (quote ("C:/Users/vj/Dropbox/org/tasks.org")))
+  (setq org-agenda-files (quote ("C:/Users/vj/Dropbox/org/tasks/")))
   ) 
 
  ((string-equal system-type "gnu/linux")
   ;; Linux settings go here
-  (setq org-agenda-files (quote ("/home/vj/Dropbox/org/tasks.org")))
+  (setq org-agenda-files (quote ("/home/vj/Dropbox/org/tasks/")))
   )
 
  )
@@ -28,8 +32,8 @@
 	     (define-key modalka-mode-map (kbd "SPC c o") #'org-clock-out);; Clock out
 	     (define-key modalka-mode-map (kbd "SPC c t") #'org-evaluate-time-range);; Clock update
 	     (define-key modalka-mode-map (kbd "SPC c r") #'org-clock-report);; Clock update
-	     (define-key modalka-mode-map (kbd "SPC e P") #'org-publish-haq)
-	     (define-key modalka-mode-map (kbd "SPC e p") #'org-publish)
+	     (define-key modalka-mode-map (kbd "SPC e P") #'org-publish-haq-all)
+	     (define-key modalka-mode-map (kbd "<f5>") #'org-publish-haq)
 	     )
 
 ;; hooking up org-bullet mode
@@ -40,7 +44,11 @@
 (setq org-todo-keywords
       '((sequence "TODO" "DOING"  "PAUSE" "DONE" "WAITING" "CANCELLED")))
 
-(setq org-todo-keyword-faces
+
+(cond
+ ((string-equal system-type "windows-nt")
+  ;; Windows settings go here
+  (setq org-todo-keyword-faces
       '(
 	("TODO" . (:background "Red"))
 	("WAITING" . (:background "Black" :foreground "White"))
@@ -49,12 +57,19 @@
 	("DONE" . (:background "Sea green"))
 	("CANCELLED" . (:foreground "Gray"))
 	))
+  ) 
+ )
+
 
 ;; Org-babel <--- Love emacs
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((ditaa . t))) ; this line activates ditaa
 (setq org-ditaa-jar-path "~/.emacs.d/ditaa/ditaa0_9.jar")
+(defun my-org-confirm-babel-evaluate (lang body)
+  (not (string= lang "ditaa")))  ;don't ask for ditaa
+(setq org-confirm-babel-evaluate #'my-org-confirm-babel-evaluate)
+
 
 
 ;; Artist mode settings
