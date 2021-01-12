600dccba8ddb48faebe25ca8d963b85b7315a582
Vj <venkatesh.jatla@gmail.com>
2021-01-12 08:22:25 -0700

    A big chagne in literrate config files

---
+++ init.el
@@ -0,0 +1,77 @@
+;; Package repositories
+(setq package-archives
+      '(
+	("gnu" . "https://elpa.gnu.org/packages/")
+	("melpa" . "https://melpa.org/packages/")
+	)
+      )
+
+;; Activating use-package (Installing if not found)
+(require 'package)
+(unless (package-installed-p 'use-package)
+(package-refresh-contents)
+  (package-install 'use-package))
+(unless (package-installed-p 'org)
+(package-refresh-contents)
+  (package-install 'org))
+
+;; All backup files are in one directory
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; Load literate configuration file
+(cond
+ ((string-equal system-type "windows-nt")
+
+  ;; The following path is necessary to get following working,
+  ;; 1. wakatime
+  ;; 2. anaconda-mode
+  (setenv "PATH" (concat (getenv "PATH") "C:/Users/vj/scoop/apps/python/current/Scripts"))
+  (setq exec-path (append exec-path '("C:/Users/vj/scoop/apps/python/current/Scripts")))
+
+  ;; Windows settings go here
+  (org-babel-load-file "literate-config/configuration.org")
+  (org-babel-load-file "literate-config/keybindings.org")
+  (org-babel-load-file "literate-config/python.org")
+  (org-babel-load-file "literate-config/web.org")
+
+
+  )
+
+
+ ;; Linux configuration
+ ((string-equal system-type "gnu/linux")
+  ;; Literate config files to load
+  (org-babel-load-file "~/Dropbox/.emacs.d/literate-config/configuration.org")
+  (org-babel-load-file "~/Dropbox/.emacs.d/literate-config/keybindings.org")
+  (org-babel-load-file "~/Dropbox/.emacs.d/literate-config/python.org")
+  (org-babel-load-file "~/Dropbox/.emacs.d/literate-config/web.org")
+
+  ;; The following path is necessary to get following working,
+  ;; 1. wakatime
+  ;; 2. anaconda-mode
+  (setenv "PATH" (concat (getenv "PATH") ":/home/vj/.local/bin"))
+  (setq exec-path (append exec-path '("/home/vj/.local/bin")))
+  )
+ )
+
+;; Setting font
+(set-frame-font "JetBrains Mono Regular 13" nil t)
+
+;; Setting theme
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(xah-fly-keys which-key wakatime-mode use-package treemacs solarized-theme smooth-scrolling smex projectile modalka magit keyfreq key-chord highlight-symbol gruvbox-theme git-gutter gcmh direx dashboard counsel company-anaconda cfml-mode all-the-icons))
+ '(wakatime-api-key "948f1aa9-1e61-46dc-8e1f-eed41c05f2fa"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- literate-config/keybindings.el
+++
@@ -1,8 +0,0 @@
-(use-package keyfreq
-:ensure t
-:config
-(keyfreq-mode 1)
-(keyfreq-autosave-mode 1)
-(setq keyfreq-exclude-commands '(self-insert-command org-cycle)))
-
-
