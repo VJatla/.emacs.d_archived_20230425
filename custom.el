c8695e4ebe5b56fb4811328d5f911947a5b341e7
vj <venkatesh.jatla@gmail.com>
2021-02-22 11:23:14 -0700

    Before giving separate theme to wsl

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(xah-fly-keys modalka key-chord keyfreq cfml-mode py-autopep8 company-anaconda anaconda-mode counsel swiper highlight-symbol wakatime-mode direx git-gutter magit treemacs projectile flycheck-popup-tip flycheck-pos-tip flycheck ivy smex company markdown-mode+ markdown-mode all-the-icons dashboard color-theme-sanityinc-tomorrow gruvbox-theme solarized-theme gcmh smooth-scrolling which-key org use-package))
+   '(org-journal xah-fly-keys modalka key-chord keyfreq cfml-mode py-autopep8 company-anaconda anaconda-mode counsel swiper highlight-symbol wakatime-mode direx git-gutter magit treemacs projectile flycheck-popup-tip flycheck-pos-tip flycheck ivy smex company markdown-mode+ markdown-mode all-the-icons dashboard color-theme-sanityinc-tomorrow gruvbox-theme solarized-theme gcmh smooth-scrolling which-key org use-package))
  '(wakatime-api-key "948f1aa9-1e61-46dc-8e1f-eed41c05f2fa"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
