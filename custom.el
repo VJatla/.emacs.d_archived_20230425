457dca41cb851c1dd217439e345c35daf313ea22
vj <venkatesh.jatla@gmail.com>
2021-05-14 21:08:38 -0600

    iedit mode is now SPC 9

--- custom.el
+++ custom.el
@@ -7,17 +7,49 @@
    ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
  '(company-quickhelp-color-background "#4F4F4F")
  '(company-quickhelp-color-foreground "#DCDCCC")
+ '(compilation-message-face 'default)
  '(conda-anaconda-home "/home/vj/anaconda3")
+ '(cua-global-mark-cursor-color "#93E0E3")
+ '(cua-normal-cursor-color "#DCDCCC")
+ '(cua-overwrite-cursor-color "#F0DFAF")
+ '(cua-read-only-cursor-color "#7F9F7F")
  '(custom-safe-themes
-   '("e6df46d5085fde0ad56a46ef69ebb388193080cc9819e2d6024c9c6e27388ba9" default))
+   '("e266d44fa3b75406394b979a3addc9b7f202348099cfde69e74ee6432f781336" "d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "e6df46d5085fde0ad56a46ef69ebb388193080cc9819e2d6024c9c6e27388ba9" default))
  '(fci-rule-color "#383838")
+ '(highlight-changes-colors '("#DC8CC3" "#bbb0cb"))
+ '(highlight-symbol-colors
+   '("#681063eb5999" "#54db645164d1" "#6098535f5323" "#5c2959a95fa1" "#4edf55f24ea4" "#64de597a525e" "#530160d26158"))
+ '(highlight-symbol-foreground-color "#FFFFEF")
+ '(highlight-tail-colors
+   '(("#4F4F4F" . 0)
+     ("#488249" . 20)
+     ("#5dacaf" . 30)
+     ("#57a2a4" . 50)
+     ("#b6a576" . 60)
+     ("#ac7b5a" . 70)
+     ("#aa5790" . 85)
+     ("#4F4F4F" . 100)))
+ '(hl-bg-colors
+   '("#b6a576" "#ac7b5a" "#9f5c5c" "#aa5790" "#85749c" "#57a2a4" "#5dacaf" "#488249"))
+ '(hl-fg-colors
+   '("#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F"))
+ '(hl-paren-colors '("#93E0E3" "#F0DFAF" "#8CD0D3" "#bbb0cb" "#7F9F7F"))
+ '(lsp-ui-doc-border "#FFFFEF")
  '(nrepl-message-colors
    '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
+ '(org-agenda-files
+   '("/home/vj/data/Dropbox/org/tasks/EMED/EMED.org" "/home/vj/data/Dropbox/org/tasks/Home/Home.org" "/home/vj/data/Dropbox/org/tasks/Research/HARP.org" "/home/vj/data/Dropbox/org/tasks/Research/MedVids.org" "/home/vj/data/Dropbox/org/tasks/Vj/Study.org" "/home/vj/data/Dropbox/org/tasks/.#Meetings.org" "/home/vj/data/Dropbox/org/tasks/Meetings.org"))
  '(package-selected-packages
    '(ivy-avy god-mode markdown-toc anaconda-mode flycheck ivy company markdown-mode apropospriate-theme kaolin-themes material-theme org zenburn-theme xah-fly-keys which-key wakatime-mode use-package treemacs sublime-themes solarized-theme smooth-scrolling smex smartparens py-autopep8 org-agenda-property monokai-pro-theme moe-theme modalka magit keyfreq key-chord jazz-theme iedit highlight-symbol gruvbox-theme git-gutter gcmh flycheck-pos-tip flycheck-popup-tip direx dashboard csv-mode counsel-projectile conda company-anaconda color-theme-sanityinc-tomorrow cfml-mode all-the-icons ace-jump-buffer))
  '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
+ '(pos-tip-background-color "#4F4F4F")
+ '(pos-tip-foreground-color "#FFFFEF")
  '(safe-local-variable-values '((TeX-master . t)))
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#7F9F7F" "#4F4F4F" 0.2))
+ '(term-default-bg-color "#3F3F3F")
+ '(term-default-fg-color "#DCDCCC")
  '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-background-mode nil)
  '(vc-annotate-color-map
    '((20 . "#BC8383")
      (40 . "#CC9393")
@@ -40,7 +72,13 @@
  '(vc-annotate-very-old-color "#DC8CC3")
  '(wakatime-api-key "948f1aa9-1e61-46dc-8e1f-eed41c05f2fa")
  '(wakatime-cli-path "/home/vj/.local/bin/wakatime")
- '(wakatime-python-bin nil))
+ '(wakatime-python-bin nil)
+ '(weechat-color-list
+   '(unspecified "#3F3F3F" "#4F4F4F" "#9f5c5c" "#CC9393" "#488249" "#7F9F7F" "#b6a576" "#F0DFAF" "#57a2a4" "#8CD0D3" "#aa5790" "#DC8CC3" "#5dacaf" "#93E0E3" "#DCDCCC" "#6F6F6F"))
+ '(xterm-color-names
+   ["#4F4F4F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#fffff6"])
+ '(xterm-color-names-bright
+   ["#3F3F3F" "#DFAF8F" "#878777" "#6F6F6F" "#DCDCCC" "#bbb0cb" "#FFFFEF" "#FFFFFD"]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
