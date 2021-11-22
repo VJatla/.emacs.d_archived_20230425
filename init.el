6b9bb150368e141da2d5c3cab56062b1231ac005
Vj <vj@xps.localdomain>
2021-11-22 10:10:30 -0700

    Added config that can work wiht xps system from ivPCL

--- init.el
+++ init.el
@@ -170,6 +170,18 @@
      );; xps-win (WSL)
 
 
+  
+    ((string-equal (upcase (system-name)) "XPS")
+   (use-package zenburn-theme
+     :ensure t)
+     (load-theme 'zenburn t)
+     (add-to-list 'default-frame-alist
+		  '(font . " JetBrainsMono Nerd Font Mono-14.5"))
+     (custom-set-variables '(wakatime-cli-path "/home/vj/.local/bin/wakatime"))
+     
+     );; XPS
+
+
     ((string-equal (system-name) "EM0456E55E16C2")
      (use-package monokai-pro-theme
        :ensure t)
