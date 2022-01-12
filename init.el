240d5cddf0e04190c5007ad30962b03d47f9683c
Vj <venkatesh.jatla@gmail.com>
2022-01-11 17:16:52 -0700

    Changes to MX@aurora

--- init.el
+++ init.el
@@ -109,7 +109,7 @@
   (setq org-agenda-files '("/home/vj/Dropbox/org/tasks"))
   (setq diary-file "/home/vj/Dropbox/org/diary-file")
   (add-to-list 'default-frame-alist
-               '(font . "JetBrains Mono-12"))
+               '(font . "LiberationMono-12"))
   );; Jolly
 
 
