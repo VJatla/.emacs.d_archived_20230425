814e8c744adb4ea961d14ee3a7d64e21127dd390
vj <venkatesh.jatla@gmail.com>
2022-05-20 10:50:13 -0600

    Now adding keybindings that are good for kinesis advantage 2

--- init.el
+++ init.el
@@ -155,7 +155,7 @@
 
   ;; Font 
   (add-to-list 'default-frame-alist
-	       '(font . "Victor Mono-14"))
+	       '(font . "Jetbrains Mono-14"))
 
   ;; Binary file paths
   (custom-set-variables '(wakatime-cli-path "/home/vj/.local/bin/wakatime"))
